2a988c27ac6bb7549ae5370e0db53f6b34ae0e43
AnthonyDos <dossantosanthony060@gmail.com>
2025-06-03 10:39:38 +0200

    modif fichier bdd

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,21 @@
 const sqlite3 = require('sqlite3');
 const path = require('path');
+const fs = require('fs');
 
-const dbPath = path.join(__dirname, 'data', 'data.sqlite');
-const db = new sqlite3.Database(dbPath);
+const dataDir = path.join(__dirname, 'data');
+
+if (!fs.existsSync(dataDir)) {
+  fs.mkdirSync(dataDir, { recursive: true });
+}
+
+const dbPath = path.join(dataDir, 'data.sqlite');
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error('Erreur lors de l\'ouverture de la base SQLite:', err.message);
+  } else {
+    console.log('Base SQLite ouverte avec succès:', dbPath);
+  }
+});
 
 db.serialize(() => {
   db.run(`
